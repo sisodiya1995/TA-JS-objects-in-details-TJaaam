d7ed72c44cbf05c3e670082d78167acb1d4e4d8e
Akash Singh <akashingh95@gmail.com>
2022-02-25 23:31:07 +0530

    quiz|app

---
+++ block-BJaaim/data.js
@@ -0,0 +1,29 @@
+var quizCollection = [
+    {
+    title :"What is captial of India"
+    ,
+    options :["Delhi" ,"Lucknow" ,"Kolkata" ,"Agra"]
+    ,
+    answerIndex : 0
+
+   }
+,
+   {
+    title :"What is the capital of Utter Predesh"
+    ,
+    options :["Lucknow" ,"Delhi" ,"Jaipur" ,"Agra"]
+    ,
+    answerIndex : 1
+
+   }
+,
+{
+    title :"What is the capital of Himanchal Pradesh"
+    ,
+    options :["Deharadun" ,"Dhramshal" ,"jaipur " ,"Delhi"]
+    ,
+    answerIndex : 2
+
+   }
+
+]
